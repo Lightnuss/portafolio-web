39100b5c6b87d9b4a99a2d244f8a09cb704bef82
Javier Vega <ja.vega@duocuc.cl>
2024-05-16 08:16:51 -0400

    Actualizacion

--- portafolio_v2/js/validaciones.js
+++ portafolio_v2/js/validaciones.js
@@ -28,20 +28,91 @@ function validarEmail() {
 }
 
 //Validacion de login (Contraseña)
-const campoContraseña = document.getElementById("password");
+function validarContraseña() {
+    const campoContraseña = document.getElementById("password");
+    const errorPassword = document.getElementById("errorPassword");
 
-campoContraseña.addEventListener("input", () => {
-    validarContraseña();
+    // Verificar si el campo de contraseña está vacío
+    if (campoContraseña.value.trim() === '') {
+        errorPassword.style.display = 'block';
+    } else {
+        errorPassword.style.display = 'none';
+    }
+}
+
+// Esperar a que el DOM esté completamente cargado antes de ejecutar el código
+document.addEventListener("DOMContentLoaded", function() {
+    // Asociar la función de validación al evento "input" del campo de contraseña
+    const campoContraseña = document.getElementById("password");
+    campoContraseña.addEventListener("input", validarContraseña);
 });
 
-function validarContraseña() {
-    const contraseña = campoContraseña.value.trim();
-    const errorPassword = document.getElementById('errorPassword');
+///Validar de Nombre
+const esVacio = (palabra) => palabra.trim() === '';
 
-    // Verifica si la contraseña está vacía
-    if (contraseña === '') {
-        errorPassword.style.display = 'block'; // Muestra el mensaje de error de contraseña requerida
+const campoNombre = document.getElementById("name");
+
+campoNombre.addEventListener("input",(evento)=>{
+    validarNombre();
+});
+
+function validarNombre() {
+    const nombre = campoNombre.value.trim();
+    const errorNombre = document.getElementById('errorNombre');
+
+    if (esVacio(nombre)) {
+        errorNombre.style.display = 'block'; // Muestra el mensaje de error
     } else {
-        errorPassword.style.display = 'none'; // Oculta el mensaje de error de contraseña requerida
+        errorNombre.style.display = 'none'; // Oculta el mensaje de error
+    }
+}
+
+///Validar de apellido
+    document.getElementById("lastName").addEventListener("blur", function() {
+        var lastNameInput = document.getElementById("lastName");
+        var errorLastName = document.getElementById("errorLastName");
+        if (lastNameInput.value.trim() === "") {
+            errorLastName.style.display = "block"; // Mostrar mensaje de validación si el campo está vacío
+        } else {
+            errorLastName.style.display = "none"; // Ocultar mensaje de validación si el campo es válido
+        }
+    });
+///Validar de Numero
+
+const campoTelefono = document.getElementById("phone");
+
+campoTelefono.addEventListener("input", () => {
+    validarTelefono();
+});
+
+function validarTelefono() {
+    const telefono = campoTelefono.value.trim();
+    const errorPhoneInvalid = document.getElementById('errorPhoneInvalid');
+
+    // Verifica si el número de teléfono contiene solo dígitos numéricos
+    if (!/^\d*$/.test(telefono)) {
+        errorPhoneInvalid.style.display = 'block'; // Muestra el mensaje de error de teléfono inválido
+    } else {
+        errorPhoneInvalid.style.display = 'none'; // Oculta el mensaje de error de teléfono inválido
+    }
+}
+
+///Validar de Mensaje
+
+const campoMensaje = document.getElementById("message");
+
+campoMensaje.addEventListener("input", () => {
+    validarMensaje();
+});
+
+function validarMensaje() {
+    const mensaje = campoMensaje.value.trim();
+    const errorMessage = document.getElementById('errorMessage');
+
+    // Verifica si el campo de mensaje está vacío
+    if (mensaje === '') {
+        errorMessage.style.display = 'block'; // Muestra el mensaje de error de mensaje requerido
+    } else {
+        errorMessage.style.display = 'none'; // Oculta el mensaje de error de mensaje requerido
     }
 }
